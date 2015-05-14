30ff43bb3c9bedfee6b71529923e7929a01c9d16
Hansol Jang <zzang.hansol@gmail.com>
2015-05-14 18:13:31 +0900

    5/14(18:13) - 아이템목록조회 추가(테스트완료)

--- models/db_items.js
+++ models/db_items.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql');
 var db_config = require('./db_config');
-
+var dao = require('./db_items_dao');
 var pool = mysql.createPool(db_config);
 
-//아이템구매가능목록조회
-exports.buyinfo = function (data, callback) {
-  var success = 1;
-  callback(success);
+/*
+  아이템록조회
+  모든 아이템목록을 주면 클라이언트에서 살 수 있는지 없는지 판단
+ */
+exports.getlist = function (data, callback) {
+  pool.getConnection(function(err, conn) {
+    if(err) {
+      callback(err);
+    }
+    dao.selectItems(conn, callback);
+  });
 };
 
 //아이템구매
